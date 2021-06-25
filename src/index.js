7ee867cf2facc940ddfc4f7d050cf175cb98cb4f
Samuel Alev <samuel.alev@protonmail.com>
2021-06-25 14:17:05 +0200

    feat: add prettier config to remove conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
-    extends: ['stylelint-config-standard', 'stylelint-config-css-modules', 'stylelint-config-idiomatic-order'],
+    extends: [
+        'stylelint-config-standard',
+        'stylelint-config-css-modules',
+        'stylelint-config-prettier',
+        'stylelint-config-idiomatic-order',
+    ],
     plugins: ['stylelint-order'],
     rules: {
         indentation: 4,
