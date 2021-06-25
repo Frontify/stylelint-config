6502081a7a797151c891c27d80af727f74e3525d
Samuel Alev <samuel.alev@protonmail.com>
2021-06-25 14:01:57 +0200

    feat: initial config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    extends: ['stylelint-config-standard', 'stylelint-config-css-modules', 'stylelint-config-idiomatic-order'],
+    plugins: ['stylelint-order'],
+    rules: {
+        indentation: 4,
+    },
+};
